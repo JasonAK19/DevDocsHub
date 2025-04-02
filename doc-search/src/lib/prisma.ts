916fe3ede080755bb9a4539942f63bd54079f592
Jason Appiah <jasona2@umbc.edu>
2025-04-02 15:28:37 -0400

    removing datasources from prisma file

--- doc-search/src/lib/prisma.ts
+++ doc-search/src/lib/prisma.ts
@@ -4,11 +4,6 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
   log: ['error', 'warn'],
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
 });
 
 if (process.env.NODE_ENV !== 'development') {
